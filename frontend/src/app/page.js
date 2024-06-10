2229769585589932d0e38a444c40d3c3eaac3821
Yotham <yothamsage@gmail.com>
2024-06-09 21:40:40 -0400

    Fine tuned device scaling a bit more
    Still need to fully flush it out for ipads / devices of that size

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <div className="overflow-auto h-screen">
       <Navbar/>
-      <section className="flex flex-col lg:flex-row justify-between items-center min-h-screen bg-cover bg-center bg-[url('/img/gradient_back_one.png')] px-6 lg:px-12 sm:py-40">
+      <section className="flex flex-col lg:flex-row justify-between items-center min-h-screen bg-cover bg-center bg-[url('/img/gradient_back_one.png')] px-6 lg:px-12 py-40">
         <div className="flex-1 text-center lg:text-left">
-          <h1 className="text-3xl lg:text-6xl font-bold text-gray-900 font-serif">
+          <h1 className="text-3xl lg:text-6xl font-bold text-gray-900 font-serif ">
             Creating nonprofit ecosystems through data.
           </h1>
           <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-md mx-auto lg:mx-0">
@@ -25,7 +25,7 @@ export default function Home() {
             </button>
           </div>
         </div>
-        <div className="flex-1 flex justify-center items-center mt-8 lg:mt-0">
+        <div className="flex-1 flex justify-center items-center mt-8 lg:mt-[-12]">
           <img draggable = {false} src="/img/node_edge_final.png" alt="Network Visualization" className="w-5/6 h-5/6 max-w-full max-h-full" />
         </div>
       </section>
@@ -38,7 +38,7 @@ export default function Home() {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12">
           <div className="flex flex-col items-center text-center">
-            <img draggable = {false} src="/img/four_bar_final.png" alt="Graph and Magnifying Glass" className="max-w-full" />
+            <img draggable = {false} src="/img/four_bar_final.png" alt="Graph and Magnifying Glass" className="w-1/2 sm:w-full max-w-full" />
           </div>
           <div className="flex flex-col justify-center text-left text-lg md:text-2xl mt-6 font-serif">
             <div className="mb-6 md:mb-10">
@@ -62,18 +62,18 @@ export default function Home() {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6 mx-auto mt-12">
           <div className="flex flex-col items-center text-center">
             <img draggable = {false} src="/img/graph_final.png" alt="Image 1" className="w-1/3 max-w-full mx-auto" />
-            <h3 className="text-xl md:text-4xl mt-4">1.7 million</h3>
-            <p className="text-sm md:text-xl mt-2 font-sans">Number of U.S. nonprofits</p>
+            <h3 className="text-2xl md:text-4xl mt-4">1.7 million</h3>
+            <p className="text-lg md:text-xl mt-2 font-sans">Number of U.S. nonprofits</p>
           </div>
-          <div className="flex flex-col items-center text-center">
+          <div className="flex flex-col items-center text-center mt-8 sm:mt-0">
             <img draggable = {false} src="/img/stack_final.png" alt="Image 2" className="w-1/3 max-w-full mx-auto" />
-            <h3 className="text-xl md:text-4xl mt-4">3</h3>
-            <p className="text-sm md:text-xl mt-2 font-sans">Data visualization platforms offered</p>
+            <h3 className="text-2xl md:text-4xl mt-4">3</h3>
+            <p className="text-lg md:text-xl mt-2 font-sans">Data visualization platforms offered</p>
           </div>
-          <div className="flex flex-col items-center text-center">
+          <div className="flex flex-col items-center text-center  mt-8 sm:mt-0 pb-24 sm:pb-0">
             <img draggable = {false} src="/img/dash_final.png" alt="Image 3" className="w-1/3 max-w-full mx-auto" />
-            <h3 className="text-xl md:text-4xl mt-4">100+</h3>
-            <p className="text-sm md:text-xl mt-2 font-sans">Algorithmic insights for key financial nonprofits variables</p>
+            <h3 className="text-2xl md:text-4xl mt-4">100+</h3>
+            <p className="text-lg md:text-xl mt-2 font-sans">Algorithmic insights for key financial nonprofits variables</p>
           </div>
         </div>
       </section>
@@ -84,7 +84,7 @@ export default function Home() {
             <div className="w-24 md:w-96 rounded-full h-1 bg-gray-900"></div>
           </div>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 gap-y-8 md:gap-6 md:gap-y-10 mx-auto mt-12">
+        <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-4 gap-y-8 md:gap-6 md:gap-y-10 mx-auto mt-12">
           <div className="flex flex-col items-center text-center">
             <a href="https://en.wikipedia.org/wiki/John_Smith_(explorer)" target="_blank" rel="noopener noreferrer">
               <img draggable = {false} src="/img/john_smith.jpg" alt="Image 1" className="max-w-full mx-auto border border-gray-900 rounded-xl even-shadow profile" />
@@ -135,12 +135,12 @@ export default function Home() {
           </div>
         </div>
       </section>   
-      <section className="flex flex-col lg:flex-row justify-between items-center min-h-screen bg-cover bg-center bg-[url('/img/gradient_two.png')] px-6 lg:px-12  sm:py-40">
+      <section className="flex flex-col lg:flex-row justify-between items-center min-h-screen bg-cover bg-center bg-[url('/img/gradient_two.png')] px-6 lg:px-12  py-40">
         <div className="flex-grow text-center lg:text-left">
           <h1 className="text-3xl lg:text-6xl font-bold text-gray-900 font-serif">
             Ready to collaborate?
           </h1>
-          <p className="text-sm md:text-2xl mt-2 font-sans">Try any of our three platforms today.</p>
+          <p className="text-lg md:text-2xl mt-2 font-sans">Try any of our three platforms today.</p>
           <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-md mx-auto lg:mx-0">
             <button className="text-lg px-6 py-2 bg-white text-gray-900 rounded-full shadow-md hover:bg-gray-200 transition">
               Data
