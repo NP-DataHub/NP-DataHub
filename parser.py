33dafdf10c73ebb7a3e73852046cff324b1871e9
Youssef-Chip <yhassan2004@gmail.com>
2024-06-14 15:09:24 -0400

    separate into functions

--- parser.py
+++ parser.py
@@ -8,38 +8,70 @@ class Database:
     def __init__(self):
         self.public_data = {}
         self.private_data = {}
+        self.root = None
+        self.namespace = {'irs': 'http://www.irs.gov/efile'}
 
-    def get_general_information(self,root,namespace):
+    def get_ein_and_tax_period(self):
+        ein_element = (self.root).find('.//irs:Filer/irs:EIN', self.namespace)
+        ein = int(ein_element.text) or None
+        # Extract Tax period
+        tax_period_element = (self.root).find('.//irs:TaxYr', self.namespace)
+        tax_period = tax_period_element.text or None
+        return ein,tax_period
 
-    def extract_non_profit_data(self,file_path):
-        # Parse the XML file
-        tree = ET.parse(file_path)
-        root = tree.getroot()
-        # Define the namespace
-        namespace = {'irs': 'http://www.irs.gov/efile'}
-        
+    def get_ntee_and_subsection(self,ein):
+        url = "https://projects.propublica.org/nonprofits/api/v2/organizations/"+ ein+".json"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            ntee  = data["organization"]["ntee_code"] or "None"
+            major_group = ntee[0];
+            subsection = data["organization"]["subsection_code"]
+            return ntee, major_group, subsection
+        else:
+            print("ERROR: Could not find the company") 
+
+    def get_general_information(self,ein):
+        name_element = (self.root).find('.//irs:Filer/irs:BusinessName/irs:BusinessNameLine1Txt', self.namespace)
+        name = name_element.text or None
+        state_element = (self.root).find('.//irs:Filer/irs:USAddress/irs:StateAbbreviationCd', self.namespace)
+        state = state_element.text or None
+        city_element = (self.root).find('.//irs:Filer/irs:USAddress/irs:CityNm', self.namespace)
+        city = city_element.text or None
+        zip_code_element = (self.root).find('.//irs:Filer/irs:USAddress/irs:ZIPCd', self.namespace)
+        zip_code = zip_code_element.text or None
+        ntee,major_group,subsection_code = self.get_ntee_and_subsection(str(ein))
+        return name,state,city,zip_code,ntee,major_group,subsection_code
+
+
+    def get_public_financial_information(self):
+        total_revenue_element = (self.root).find('.//irs:TotalRevenueGrp/irs:TotalRevenueColumnAmt', self.namespace)
+        total_revenue = total_revenue_element.text or None
+        return total_revenue
+
+    def build_database(self,file_path):
+        self.root = (ET.parse(file_path)).getroot()
+
         #Check for public or private
-        return_type_element = root.find('.//irs:ReturnTypeCd', namespace)
-        return_type = return_type_element.text if return_type_element is not None else None
+        return_type_element = (self.root).find('.//irs:ReturnTypeCd', self.namespace)
+        return_type = return_type_element.text or None
 
         if return_type == "990PF":
             print("tkt")
         else :
-            # Extract EIN keep it string and add check for 0 in begining if too short
-            ein_element = root.find('.//irs:Filer/irs:EIN', namespace)
-            ein = int(ein_element.text) if ein_element is not None else None
-            # Extract Tax period
-            tax_period_element = root.find('.//irs:TaxYr', namespace)
-            tax_period = tax_period_element.text if tax_period_element is not None else None
+            ein,tax_period = self.get_ein_and_tax_period()
             if ein not in self.public_data:
-                # Extract Name of Organization
-                name_organization_element = root.find('.//irs:Filer/irs:BusinessName/irs:BusinessNameLine1Txt', namespace)
-                name_organization = name_organization_element.text if name_organization_element is not None else None
-                # Extract TotalRevenue
-                total_revenue_element = root.find('.//irs:TotalRevenueGrp/irs:TotalRevenueColumnAmt', namespace)
-                total_revenue = total_revenue_element.text if total_revenue_element is not None else None
+                name,state,city,zip_code,ntee,major_group,subsection_code = self.get_general_information(ein)
+                total_revenue = self.get_public_financial_information()
                 self.public_data[ein] = {
-                    "Name": name_organization,
+                    "Name": name,
+                    "City": city,
+                    "State": state,
+                    "Zipcode": zip_code,
+                    "NTEE" : ntee,
+                    "Major Group": major_group,
+                    "Subsection code": subsection_code,
                     tax_period: {
                         "Total Revenue": total_revenue
                     }
@@ -58,29 +90,16 @@ class Database:
         for filename in os.listdir(directory):
             if filename.endswith('.xml'):
                 file_path = os.path.join(directory, filename)
-                self.extract_non_profit_data(file_path)
+                self.build_database(file_path)
 
     def insert_data_into_mongodb(self, db_name, collection_name):
         # Connect to MongoDB
         client = MongoClient("mongodb+srv://youssef:TryAgain@youssef.bl2lv86.mongodb.net/?retryWrites=true&w=majority&appName=Youssef")
         database = client[db_name]
         collection = database[collection_name]
-
         # Insert data into the collection
         collection.insert_many(self.public_data)
 
-    def get_from_api(ein):
-        url = "https://projects.propublica.org/nonprofits/api/v2/organizations/"+ein+".json"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            data = response.json()
-            ntee  = data["organization"]["ntee_code"]
-            ntee_section = ntee[0];
-            subsection = data["organization"]["subsection_code"]
-            return [ntee, ntee_section, subsection]
-        else:
-            print("ERROR: Could not find the company") 
 
 if __name__ == "__main__":
 
@@ -89,7 +108,6 @@ if __name__ == "__main__":
     obj = Database()
     # Process all XML files and store data in a list
     obj.process_all_xml_files(directory)
-
     # Insert the data into MongoDB
     #insert_data_into_mongodb(all_data, 'Test', 'test')
     lst  = []
@@ -99,12 +117,4 @@ if __name__ == "__main__":
         new_dict.update(details)
         lst.append(new_dict)
 
-    for company in lst:
-        print(f"EIN: {company['EIN']}")
-        print(f"Name: {company['Name']}")
-        for year in sorted(key for key in company.keys() if key.isdigit()):
-            print(f"  {year}:")
-            print(f"    Revenue: {company[year]['Total Revenue']}")
-        print()  # Blank line for readability between organizations
-
-    #print("Data has been successfully inserted into MongoDB.")
+    print("Data has been successfully inserted into MongoDB.")
