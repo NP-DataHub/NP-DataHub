d6509d00429a76000f2fe71a844a54762cb8a314
Thomas Orifici <thomas.orifici@gmail.com>
2024-06-14 22:51:06 -0400

    scaling, etc

--- frontend/src/app/components/BasicBarChart.js
+++ frontend/src/app/components/BasicBarChart.js
@@ -3,7 +3,20 @@
 import React from 'react';
 import ReactECharts from 'echarts-for-react';
 
-const BasicBarChart = () => {
+
+/**
+ * @param variable - string containing the variable recorded in the graph
+ * @param values -   an array of values measured each year
+ * @param style -    a struct containing format options - height and width
+ *                   must be defined as numbers.
+*/
+const BasicBarChart = ({variable, values, style}) => {
+  // ensures arg is an array
+  if (!Array.isArray(values) || values.length === 0) {
+    return <div>ERROR: chart arg must be an array</div>;
+  }
+  let scale = Math.round((style.width+style.height)/2)
+
   const option = {
     tooltip: {
       trigger: 'axis',
@@ -19,42 +32,45 @@ const BasicBarChart = () => {
       }
     },
     grid: {
-      left: '8%',
-      right: '4%',
-      bottom: '8%',
+      left: Math.round(0.06*scale),
+      bottom: Math.round(0.06*scale),
       containLabel: true
     },
     xAxis: [
       {
-        name: 'Day',
+        name: 'Year',
         nameLocation: 'middle',
         nameTextStyle: {
           fontWeight: 'bold',
-          fontSize: 16,
-          padding: 20
+          fontSize: Math.round(0.03*scale),
+          padding: Math.round(0.03*scale)
         },
         type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        data: Array.from({ length: values.length }, (_, index) => index + 2017),
         axisTick: {
           alignWithLabel: true
+        },
+        axisLabel: {
+          fontSize: Math.round(0.018*scale)
         }
       }
     ],
     yAxis: [
       {
-        name: 'Revenue',
+        name: variable,
         type: 'value',
         nameLocation: 'middle',
         nameRotate: 90,
         nameTextStyle: {
           fontWeight: 'bold',
-          fontSize: 16,
-          padding: 50
+          fontSize: Math.round(0.03*scale),
+          padding: Math.round(0.03*scale)
         },
         axisLabel: {
           formatter: function (value) {
             return '$' + value;
-          }
+          },
+          fontSize: Math.round(0.018*scale)
         }
       }
     ],
@@ -63,13 +79,13 @@ const BasicBarChart = () => {
         name: 'Direct',
         type: 'bar',
         barWidth: '80%',
-        data: [10, 52, 200, 334, 390, 330, 220]
+        data: values
       }
     ]
   };
 
   return (
-    <ReactECharts option={option} style={{ height: '400px', width: '100%' }} />
+    <ReactECharts option={option} style={style} />
   );
 };
 
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,6 +4,9 @@ import dynamic from 'next/dynamic';
 const BasicBarChart = dynamic(() => import('./components/BasicBarChart'), { ssr: false });
 
 export default function Home() {
+  const variable = "Revenue"
+  const values = [52,64,73,67,86,95];
+  const style = { height: 200, width: 200 }
   return (
     <div className="overflow-auto h-screen">
       <Navbar/>
@@ -12,7 +15,7 @@ export default function Home() {
         <div className="flex flex-col justify-center items-center text-center py-40">
           <h2 className="text-2xl md:text-5xl mb-4 md:mb-6">Chart Testing Page :)</h2>
           <h1>Basic Bar Chart</h1>
-          <BasicBarChart />
+          <BasicBarChart variable={variable} values={values} style={style}/>
         </div>
       </section>
     </div>
