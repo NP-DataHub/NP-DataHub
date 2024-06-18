8cc35ea6a339ec762bc058d0879ce935183d2ed8
Thomas Orifici <thomas.orifici@gmail.com>
2024-06-18 13:31:45 -0400

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -6,7 +6,7 @@ const BasicBarChart = dynamic(() => import('./components/BasicBarChart'), { ssr:
 export default function Home() {
   const variable = "Revenue"
   const values = [52,64,73,67,86,95];
-  const style = { height: 200, width: 200 }
+  const style = { height: 400, width: 400 }
   return (
     <div className="overflow-auto h-screen">
       <Navbar/>
@@ -14,7 +14,7 @@ export default function Home() {
       <section className="min-h-screen bg-white text-gray-900 px-6 md:px-12 font-serif">
         <div className="flex flex-col justify-center items-center text-center py-40">
           <h2 className="text-2xl md:text-5xl mb-4 md:mb-6">Chart Testing Page :)</h2>
-          <h1>Basic Bar Chart</h1>
+          <h1>To do: add percentage functions for bar charts</h1>
           <BasicBarChart variable={variable} values={values} style={style}/>
         </div>
       </section>
