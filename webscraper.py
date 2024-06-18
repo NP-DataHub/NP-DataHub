823ba0cee20b1ffab951a0c27aaa34f48e4a1136
troyschipf <134092937+troyschipf@users.noreply.github.com>
2024-06-18 14:58:49 -0400

    Add files via upload
    Added the ability to fully download and unzip all files from the website locally

--- webscraper.py
+++ webscraper.py
@@ -1,19 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-import zipfile
+from zipfile import ZipFile
 import io
+from urllib.request import urlretrieve
 
-def extract_link(string, start_del, end_del):
-    start_index = string.find(start_del) + len(start_del)
-    end_index = string.find(end_del, start_index)
-    return string[start_index:end_index]
+def changeToZIP(fp):
+    base, ext = os.path.splitext(fp)
+    if ext != '.zip':
+        newFP = base + '.zip'
+        os.rename(fp, newFP)
 
-def extractFilesFromZip(ziplink):
-	r = requests.get(ziplink)
-	z = zipfile.ZipFile(io.BytesIO(r.content))
-	zipfile.extractall("C:/Users/troys.LAPTOP-C5UOFQ8K/OneDrive - Rensselaer Polytechnic Institute/RPI/RCOS/NP-DataHub/ZIPfolders")
-		
+def openAndDownload(ziplink):
+    path = os.path.join(os.getcwd(), 'ZIPfolders')
+    print(f"Current working directory: {os.getcwd()}")
+    print(f"Target directory: {path}")
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f"Created directory: {path}")
+
+    os.chdir(path)
+    print("Path changed to:", os.getcwd())
+
+    try:
+        r = requests.get(ziplink, allow_redirects=True)
+        r.raise_for_status()
+        filename = ziplink.split('/')[-1]
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+        print(f"Downloaded and saved as: {filename}")
+        with ZipFile(filename, 'r') as zObject:
+        	zObject.extractall(path)
+    except Exception as e:
+        print(f"Error: {e}")
+        exit()
+
 def extractZipsFromHtml():
 	url = "https://www.irs.gov/charities-non-profits/form-990-series-downloads"
 	response = requests.get(url)
@@ -22,9 +44,8 @@ def extractZipsFromHtml():
 	zip_links = soup.select('a[href$=".zip"]')
 	for ziplink in zip_links:
 		zipfilelink = ziplink['href']
-        print(zipfilelink)
-        # if not zipfilelink.startswith("http"):
-            # zipfilelink = "https://www.irs.gov" + zipfilelink
+		openAndDownload(zipfilelink)
+		exit()
 
 def main():
 	extractZipsFromHtml()
