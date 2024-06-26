122d0bf01e96d828e59d5a46e9de3a9fedc33c5c
Thomas Orifici <thomas.orifici@gmail.com>
2024-06-26 13:31:16 -0400

    Revert "Merge branch 'main' into Chart-Testing"
    This reverts commit a6899bee801984ab11db93d3f719b7a71ed733fd, reversing
    changes made to 6689bfdf9921bdad438f36aa3ff850d4357a2db5.

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "./components/context"; 
-import Home from "./Home"; 
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,12 +10,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dashboard-color">
-      <body className={inter.className}>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
-      </body>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
