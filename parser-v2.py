4bce202a65025f49d7ecb35a743ca023f595b8a2
Youssef-Chip <yhassan2004@gmail.com>
2024-06-29 22:18:40 -0400

    remove api calls, add filepath to database, add edge case for foreign address

--- parser-v2.py
+++ parser-v2.py
@@ -14,7 +14,6 @@ class Database:
             "990EZ": self.database["EZ"],
             "990PF": self.database["Private"]
         }
-        self.cache = {}
 
     def get_ein_and_tax_period(self, root):
         ein_element = root.find('.//irs:Filer/irs:EIN', self.namespace)
@@ -24,31 +23,23 @@ class Database:
         tax_period = tax_period_element.text if tax_period_element is not None else "None"
         return ein, tax_period
 
-    def get_ntee_and_subsection(self, ein):
-        # if ein in self.cache:
-        #     return self.cache[ein]
-        return "None", "None", "None"
-        url = f"https://projects.propublica.org/nonprofits/api/v2/organizations/{ein}.json"
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            ntee = data["organization"]["ntee_code"] if data["organization"]["ntee_code"] else "None"
-            major_group = ntee[0] if ntee else "None"
-            subsection = data["organization"]["subsection_code"] if data["organization"]["subsection_code"] else "None"
-            self.cache[ein] = (ntee, major_group, subsection)
-            return ntee, major_group, subsection
-        else:
-            return "None", "None", "None"
-
     def get_general_information(self, root):
         name_element = root.find('.//irs:Filer/irs:BusinessName/irs:BusinessNameLine1Txt', self.namespace)
         name = name_element.text if name_element is not None else "None"
         state_element = root.find('.//irs:Filer/irs:USAddress/irs:StateAbbreviationCd', self.namespace)
-        state = state_element.text if state_element is not None else "None"
-        city_element = root.find('.//irs:Filer/irs:USAddress/irs:CityNm', self.namespace)
-        city = city_element.text if city_element is not None else "None"
-        zip_code_element = root.find('.//irs:Filer/irs:USAddress/irs:ZIPCd', self.namespace)
-        zip_code = zip_code_element.text if zip_code_element is not None else "None"
+        if (state_element is not None):
+            state = state_element.text
+            city_element = root.find('.//irs:Filer/irs:USAddress/irs:CityNm', self.namespace)
+            city = city_element.text
+            zip_code_element = root.find('.//irs:Filer/irs:USAddress/irs:ZIPCd', self.namespace)
+            zip_code = zip_code_element.text
+        else : #Foreign Address
+            state_element = root.find('.//irs:Filer/irs:ForeignAddress/irs:CountryCd', self.namespace)
+            state = state_element.text if state_element is not None else "None"
+            city_element = root.find('.//irs:Filer/irs:ForeignAddress/irs:CityNm', self.namespace)
+            city = city_element.text if city_element is not None else "None"
+            zip_code_element = root.find('.//irs:Filer/irs:ForeignAddress/irs:ForeignPostalCd', self.namespace)
+            zip_code = zip_code_element.text if zip_code_element is not None else "None"
         return name, state, city, zip_code
 
     def get_financial_information(self, root, return_type):
@@ -57,26 +48,22 @@ class Database:
             total_assets_element = root.find('.//irs:TotalAssetsGrp/irs:EOYAmt', self.namespace)
             total_liabilities_element = root.find('.//irs:TotalLiabilitiesGrp/irs:EOYAmt', self.namespace)
             total_expenses_element = root.find('.//irs:TotalFunctionalExpensesGrp/irs:TotalAmt', self.namespace)
-            total_contributons_amt = root.find('.//irs:TotalContributionsAmt', self.namespace)
         elif return_type == "990EZ":
             total_revenue_element = root.find('.//irs:TotalRevenueAmt', self.namespace)
             total_assets_element = root.find('.//irs:Form990TotalAssetsGrp/irs:EOYAmt', self.namespace)
             total_liabilities_element = root.find('.//irs:SumOfTotalLiabilitiesGrp/irs:EOYAmt', self.namespace)
             total_expenses_element = root.find('.//irs:TotalExpensesAmt', self.namespace)
-            total_contributons_amt = root.find('.//irs:TotalContributionsAmt', self.namespace)
         elif return_type == "990PF":
             total_revenue_element = root.find('.//irs:TotalRevAndExpnssAmt', self.namespace)
             total_assets_element = root.find('.//irs:TotalAssetsEOYAmt', self.namespace)
-            total_liabilities_element = root.find('.//irs:DividendsRevAndExpnssAmt/irs:EOYAmt', self.namespace) #its not liabilities, its
+            total_liabilities_element = root.find('.//irs:TotalLiabilitiesEOYAmt', self.namespace) #its not liabilities, its
             total_expenses_element = root.find('.//irs:TotalExpensesRevAndExpnssAmt', self.namespace)
-            total_contributons_amt = root.find('.//irs:TotalContributionsAmt', self.namespace)
 
         total_revenue = int(total_revenue_element.text) if total_revenue_element is not None else 0
         total_assets = int(total_assets_element.text) if total_assets_element is not None else 0
         total_liabilities = int(total_liabilities_element.text) if total_liabilities_element is not None else 0
         total_expenses = int(total_expenses_element.text) if total_expenses_element is not None else 0
-        total_contributions = int(total_contributions_amt.text) if total_contributons_amt is not None else 0
-        return total_revenue, total_assets, total_liabilities, total_expenses, total_contributons
+        return total_revenue, total_assets, total_liabilities, total_expenses
 
     def build_database(self, file_path):
         root = ET.parse(file_path).getroot()
@@ -91,22 +78,22 @@ class Database:
             return None, None
 
         name, state, city, zip_code = self.get_general_information(root)
-        ntee, major_group, subsection_code = self.get_ntee_and_subsection(ein)
-        total_revenue, total_assets, total_liabilities, total_expenses, total_contributons = self.get_financial_information(root, return_type)
+        ntee, major_group = 'None', 'Z'
+        total_revenue, total_assets, total_liabilities, total_expenses = self.get_financial_information(root, return_type)
 
         update_fields = {
             "Name": name,
             "City": city,
             "State": state,
             "Zipcode": zip_code,
+            "EIN": ein,
             "NTEE": ntee,
             "Major Group": major_group,
-            "Subsection code": subsection_code,
+            "Filepath" : file_path[36:],
             f"{tax_period}.Total Revenue": total_revenue,
             f"{tax_period}.Total Assets": total_assets,
             f"{tax_period}.Total Liabilities": total_liabilities,
             f"{tax_period}.Total Expenses": total_expenses,
-            f"{tax_period}.Total Contributions": total_contributons
         }
 
         insertion = UpdateOne(
@@ -116,7 +103,7 @@ class Database:
         )
         return return_type, insertion
 
-    def process_all_xml_files(self, directory):
+    def process_all_xml_files(self,directory):
         num_cores = os.cpu_count()
         insertions = {"990": [], "990EZ": [], "990PF": []}
         with ThreadPoolExecutor(max_workers=num_cores) as executor:
@@ -144,7 +131,8 @@ class Database:
         collection.bulk_write(operations)
 
 if __name__ == "__main__":
-    directory = '/Users/mr.youssef/Desktop/2018-may'
+    #Change index for Filepath everytime before running script
+    directory = '/Users/mr.youssef/Desktop/NpDatahub/unitTesting'
     obj = Database()
     obj.process_all_xml_files(directory)
     print("Data has been successfully inserted into MongoDB.")
