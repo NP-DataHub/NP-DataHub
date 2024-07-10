6a6844dbddd28ece4c86a5e6b0d0aec266c2ae86
Yotham <yothamsage@gmail.com>
2024-07-10 16:55:27 -0400

    Initial Chart Sizing Fix
    This still needs to be completed. In this change I added in useEffect and Usestate to dyanmically manage the size of the chart container, and offset it based on the dimensions of the page.

--- frontend/src/app/nonprofit/page.js
+++ frontend/src/app/nonprofit/page.js
@@ -17,7 +17,6 @@ export default function non_profit() {
     const expenses = [70,55,58,63,69,80];
     const assets = [42,43,45,47,47,49];
     const liabilities = [41,42,41,43,45,43];
-    const style = { width: '100%', height: '100%' };
 
     const indicators = [
         { value: '$245K', label: 'REVENUES', barColor: 'bg-[#A6F7E2]', ranking: '74%', diff: '+6.5%', diffColor: 'border-2 border-[#029F04] bg-[#A6F7E2]' },
@@ -25,8 +24,7 @@ export default function non_profit() {
         { value: '$546K', label: 'ASSETS', barColor: 'bg-[#FFE5A5]', ranking: '47%', diff: '-1.9%', diffColor: 'border-2 border-[#6A1701] bg-[#171821]' },
         { value: '$511K', label: 'LIABILITIES', barColor: 'bg-[#C7FFA5]', ranking: '23%', diff: '-2.9%', diffColor: 'border-2 border-[#6A1701] bg-[#171821]' },
         { value: '$511K', label: 'FUNDRAISING', barColor: 'bg-[#F8A5FF]', ranking: '23%', diff: '-2.9%', diffColor: 'border-2 border-[#6A1701] bg-[#171821]' },
-
-      ];
+    ];
 
     const settings = {
         dots: true,
@@ -66,12 +64,12 @@ export default function non_profit() {
         <div>
             <div className="flex dashboard-color text-white font-sans">
                 <Sidebar />
-                <div className="flex-col w-screen overflow-x-hidden ">
+                <div className="flex-col w-full overflow-x-hidden">
                     <DashboardNavbar />
                     <div className="flex-col px-10 bg-[#21222D] rounded-md mx-10 p-10 font-sans">
                         <h1 className="text-2xl font-semibold">COMMUNITY FOUNDATION OF TROY</h1>
                         <span className="text-sm text-[#A0A0A0]">123 Main Street, Troy, New York  12180</span>
-                        <div className="mt-6 ">
+                        <div className="mt-6">
                             <Slider {...settings}>
                                 {indicators.map((indicator, index) => {
                                     const isPositive = indicator.diff.includes('+');
@@ -92,41 +90,35 @@ export default function non_profit() {
                         </div>
                     </div>
                     <div className="flex-col mx-10 font-sans">
-                        <div className="grid grid-cols-3 gap-4 mt-10 h-400px">
-                            <div className="bg-[#21222D] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-                                {/*add chart here box size will update with chart*/}
-                                <h1 style={{ textAlign: 'center', fontSize: '2em', fontWeight: 'bold' }}>Revenue</h1>
-                                <div className="flex items-center justify-center mb-12" style={{ width: '100%', height: '100%' }}>
-                                    <BarChart values={revenues} style={style}/>
+                        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10">
+                            <div className="bg-[#21222D] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 flex-grow">
+                                <h1 className="text-center text-2xl font-bold">Revenue</h1>
+                                <div className="flex items-center justify-center h-full">
+                                    <BarChart values={revenues} />
                                 </div>
                             </div>
-                            <div className="bg-[#21222D] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-                                {/*add chart here box size will update with chart*/}
-                                <h1 style={{ textAlign: 'center', fontSize: '2em', fontWeight: 'bold' }}>Revenue vs. Expenses</h1>
-                                <div className="flex items-center justify-center" style={{ width: '100%', height: '100%' }}>
-                                    <LineCompareChart variable1="Revenue" variable2="Expenses" values1={revenues} values2={expenses} style={style}/>
+                            <div className="bg-[#21222D] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 flex-grow">
+                                <h1 className="text-center text-2xl font-bold">Revenue vs. Expenses</h1>
+                                <div className="flex items-center justify-center h-full">
+                                    <LineCompareChart variable1="Revenue" variable2="Expenses" values1={revenues} values2={expenses} />
                                 </div>
                             </div>
-                            <div className="bg-[#21222D] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-                                {/*add chart here box size will update with chart*/}
-                                <h1 style={{ textAlign: 'center', fontSize: '2em', fontWeight: 'bold' }}>Overall Growth</h1>
-                                <div className="flex items-center justify-center" style={{ width: '100%', height: '100%' }}>
-                                    <StackChart revenues={revenues} expenses={expenses} assets={assets} liabilities={liabilities} style={style}/>
+                            <div className="bg-[#21222D] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 flex-grow">
+                                <h1 className="text-center text-2xl font-bold">Overall Growth</h1>
+                                <div className="flex items-center justify-center h-full">
+                                    <StackChart revenues={revenues} expenses={expenses} assets={assets} liabilities={liabilities} />
                                 </div>
                             </div>
                         </div>
-                        <div className="grid grid-cols-3 gap-4 mt-10 mb-10">
-                            <div className="bg-[#21222D] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 col-span-2">
-                                {/*add chart here box size will update with chart*/}
-                                <h1 style={{ textAlign: 'center', fontSize: '2em', fontWeight: 'bold' }}>Time Series</h1>
-                                <div className="flex items-center justify-center mb-12" style={{ width: '100%', height: '100%' }}>
-                                    <TimeSeries variable="Revenue" values={revenues} style={style} />
+                        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10 mb-10">
+                            <div className="bg-[#21222D] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 col-span-1 lg:col-span-2">
+                                <h1 className="text-center text-2xl font-bold">Time Series</h1>
+                                <div className="flex items-center justify-center h-full">
+                                    <TimeSeries variable="Revenue" values={revenues} />
                                 </div>
-
                             </div>
-                            <div className="bg-[#21222D] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-                                {/*add chart here box size will update with chart*/}
-                                <h1 style={{ textAlign: 'center', fontSize: '2em', fontWeight: 'bold' }}>Compared to State</h1>
+                            <div className="bg-[#21222D] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 flex-grow">
+                                <h1 className="text-center text-2xl font-bold">Compared to State</h1>
                             </div>
                         </div>
                     </div>
