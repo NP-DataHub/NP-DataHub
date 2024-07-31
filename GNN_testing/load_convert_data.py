7adb25e1a03a52fb31f3bafa3010b69132c403ca
Emmet Whitehead <whitee6@rpi.edu>
2024-07-31 12:40:34 -0400

    Created new script for graph creation
    Now includes rev, exp, asts, liabs, and removed "useless" info for training (EIN, id). Additionally, figured out how to not be a goon with the URI.

--- GNN_testing/load_convert_data.py
+++ GNN_testing/load_convert_data.py
@@ -5,14 +5,17 @@ import networkx as nx
 from sklearn.preprocessing import LabelEncoder
 import os
 from progress.bar import Bar
+import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 os.environ['DGLBACKEND'] = 'pytorch'  # tell DGL what backend to use, not really used but it complains if you dont
 
 # Connect to MongoDB
-uri = "whoops"
+uri = open("MONGO_DB_URI.txt", "r").read()
 client = pymongo.MongoClient(uri)
 database = client['Np-Datahub']
-collection = database['EZ']
+collection = database['NonProfitData']
 
 # NetworkX digraph for eventual conversion to DGL graph
 G = nx.DiGraph()
@@ -21,10 +24,11 @@ G = nx.DiGraph()
 name_encoder = LabelEncoder()
 city_encoder = LabelEncoder()
 state_encoder = LabelEncoder()
+ntee_encoder = LabelEncoder()
 
 
 # Grab nodes from the collection
-num_test_nodes = 130 # for initial 5 simulated NTEE codes
+num_test_nodes = 1000
 print(f"Grabbing the first {num_test_nodes} nodes from the collection")
 
 nonprofits = list(collection.find().limit(num_test_nodes))
@@ -32,37 +36,121 @@ nonprofits = list(collection.find().limit(num_test_nodes))
 names = []
 cities = []
 states = []
+ntees = []
 
 for i in nonprofits:
-    names.append(i['Name'])
-    cities.append(i['City'])
-    states.append(i['State'])
+    names.append(i['Nm'])
+    cities.append(i['Cty'])
+    states.append(i['St'])
+    # For now, ignore the subcode and use only the main code, ie the first char in the ntee code
+    ntees.append(i['NTEE'][0])
 
 # Fit the encoders
 print("Fitting the encoders")
 name_encoder.fit(names)
 city_encoder.fit(cities)
 state_encoder.fit(states)
+ntee_encoder.fit(ntees)
+
+# Determine the maximum length of the financial attributes
+max_length = max(
+    max(len([year_data['TotRev'] for year, year_data in cur_np.items() if year.isdigit() and 'TotRev' in year_data]) for cur_np in nonprofits),
+    max(len([year_data['TotExp'] for year, year_data in cur_np.items() if year.isdigit() and 'TotExp' in year_data]) for cur_np in nonprofits),
+    max(len([year_data['TotLia'] for year, year_data in cur_np.items() if year.isdigit() and 'TotLia' in year_data]) for cur_np in nonprofits),
+    max(len([year_data['TotAst'] for year, year_data in cur_np.items() if year.isdigit() and 'TotAst' in year_data]) for cur_np in nonprofits)
+)
 
 # Create the nodes using the encoders and the data
-bar = Bar('Creating Nodes from MongoDB', max = num_test_nodes)
+bar = Bar('Creating Nodes from MongoDB', max=num_test_nodes)
 for cur_np in nonprofits:
+    name_encoded = name_encoder.transform([cur_np['Nm']])[0]
     attributes = {
-        'Name': name_encoder.transform([cur_np['Name']])[0],
-        'EIN': int(cur_np['EIN']),
-        'City': city_encoder.transform([cur_np['City']])[0],
-        'State': state_encoder.transform([cur_np['State']])[0],
-        'Zipcode': int(cur_np['Zipcode']),
-        '_id': hash(cur_np['_id'])
+        'City': city_encoder.transform([cur_np['Cty']])[0],
+        'State': state_encoder.transform([cur_np['St']])[0],
+        'Zipcode': int(cur_np['Zip']),
+        'NTEE': ntee_encoder.transform([cur_np['NTEE'][0]])[0],
+        'Revs': np.pad([year_data['TotRev'] for year, year_data in cur_np.items() if year.isdigit() and 'TotRev' in year_data], (0, max_length - len([year_data['TotRev'] for year, year_data in cur_np.items() if year.isdigit() and 'TotRev' in year_data]))),
+        'Exps': np.pad([year_data['TotExp'] for year, year_data in cur_np.items() if year.isdigit() and 'TotExp' in year_data], (0, max_length - len([year_data['TotExp'] for year, year_data in cur_np.items() if year.isdigit() and 'TotExp' in year_data]))),
+        'Liabs': np.pad([year_data['TotLia'] for year, year_data in cur_np.items() if year.isdigit() and 'TotLia' in year_data], (0, max_length - len([year_data['TotLia'] for year, year_data in cur_np.items() if year.isdigit() and 'TotLia' in year_data]))),
+        'Asts': np.pad([year_data['TotAst'] for year, year_data in cur_np.items() if year.isdigit() and 'TotAst' in year_data], (0, max_length - len([year_data['TotAst'] for year, year_data in cur_np.items() if year.isdigit() and 'TotAst' in year_data])))
     }
-    G.add_node(cur_np['_id'], **attributes)
+    G.add_node(name_encoded, **attributes)
     bar.next()
 bar.finish()
 
-# create the dgl graph
+# Print the first 5 nodes in the graph, with their labels as well
+print("Sample of the first 5 nodes in the graph:")
+for node_id in list(G.nodes)[:5]:
+    print(f"Node {node_id}: {G.nodes[node_id]}")
+
+
+
+
+
+
+# create edges between the nodes:
+# connect the nodes with edges if they share an NTEE code
+bar = Bar('Creating Edges between the nodes', max=num_test_nodes)
+for cur_np in nonprofits:
+    name_encoded = name_encoder.transform([cur_np['Nm']])[0]
+    ntee = ntee_encoder.transform([cur_np['NTEE'][0]])[0]
+    for node_id in list(G.nodes):
+        if G.nodes[node_id]['NTEE'] == ntee and node_id != name_encoded:
+            G.add_edge(name_encoded, node_id)
+    bar.next()
+bar.finish()
+
+
+
+
+
+
+
+
+
+# Create the DGL graph
 print("Creating the DGL graph")
-dgl_G = dgl.from_networkx(G, node_attrs=['Name', 'EIN', 'City', 'State', 'Zipcode', '_id'])
+dgl_G = dgl.from_networkx(G, node_attrs=['City', 'State', 'Zipcode', 'NTEE', 'Revs', 'Exps', 'Liabs', 'Asts'])
 
 # Save the graph
 print("Saving the graph")
-dgl.save_graphs("EZ.dgl", dgl_G)
+dgl.save_graphs("REAL.dgl", dgl_G)
+
+
+# print the networkx graph in graph format
+plt.figure(figsize=(10, 10))
+nx.draw(G, with_labels=True)
+plt.show()
+
+
+# # Create the nodes using the encoders and the data
+# bar = Bar('Creating Nodes from MongoDB', max = num_test_nodes)
+# for cur_np in nonprofits:
+#     name_encoded = name_encoder.transform([cur_np['Nm']])[0]
+#     attributes = {
+#         #'Name': name_encoder.transform([cur_np['Nm']])[0],
+#         'City': city_encoder.transform([cur_np['Cty']])[0],
+#         'State': state_encoder.transform([cur_np['St']])[0],
+#         'Zipcode': int(cur_np['Zip']),
+#         'NTEE': ntee_encoder.transform([cur_np['NTEE'][0]])[0],
+#         'Revs': np.array([year_data['TotRev'] for year, year_data in cur_np.items() if year.isdigit() and 'TotRev' in year_data]),
+#         'Exps': np.array([year_data['TotExp'] for year, year_data in cur_np.items() if year.isdigit() and 'TotExp' in year_data]),
+#         'Liabs': np.array([year_data['TotLia'] for year, year_data in cur_np.items() if year.isdigit() and 'TotLia' in year_data]),
+#         'Asts': np.array([year_data['TotAst'] for year, year_data in cur_np.items() if year.isdigit() and 'TotAst' in year_data])
+#     }
+#     G.add_node(name_encoded, **attributes)
+#     bar.next()
+# bar.finish()
+
+# # print the first 5 nodes in the graph, with their labels as well
+# print("Sample of the first 5 nodes in the graph:")
+# for node_id in list(G.nodes)[:5]:
+#     print(f"Node {node_id}: {G.nodes[node_id]}")
+
+# # create the dgl graph
+# print("Creating the DGL graph")
+# dgl_G = dgl.from_networkx(G, node_attrs=['City', 'State', 'Zipcode', 'NTEE', 'Revs', 'Exps', 'Liabs', 'Asts'])
+
+# # Save the graph
+# print("Saving the graph")
+# dgl.save_graphs("REAL.dgl", dgl_G)
