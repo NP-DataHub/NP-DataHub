dc1331a1c8ab6f955e1ad8837398af7e12074933
caljack8 <caljack8@gmail.com>
2024-08-06 14:22:43 -0400

    Added imports for the sidebar and NavBar

--- frontend/src/pages/regional/RegionalHealth.js
+++ frontend/src/pages/regional/RegionalHealth.js
@@ -1,4 +1,6 @@
 "use client";
+import Sidebar from "@/app/components/Sidebar";
+import DashboardNavbar from "@/app/components/dashboardNav";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import { useEffect } from 'react';
