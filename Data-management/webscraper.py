40f4853fa7aca7acd9c3bffe7223cd8cf60c6736
Youssef-Chip <yhassan2004@gmail.com>
2024-10-04 16:34:48 -0400

    Complete webscraper
    Make Webscraper be able to download, unzip and call main database script on newly posted IRS files.

--- Data-management/webscraper.py
+++ Data-management/webscraper.py
@@ -1,23 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import shutil
 from zipfile import ZipFile
 from dotenv import load_dotenv
 from pymongo import MongoClient, UpdateOne
-
+from Database import Database
+import random
 
 class UpdateDatabase:
     #NOTE: THIS ONLY WORKS ASSUMING IRS DOESN'T CHANGE THE PREVIOUS URL LINKS, if they do, it will unnecessarily excecute main parser on already parsed folders
 
     def __init__(self):
-        load_dotenv('../frontend/.env')
+        load_dotenv('frontend/.env')
         self.mongo_client = MongoClient(os.getenv('MONGODB_URI'))
         self.database = self.mongo_client["Nonprofitly"]
         self.processed_folders = set()
         self.unprocessed_folders = list()
-
+        self.created_files = []
+        self.created_folders = []
     def get_already_parsed_folders(self):
-        with open('zip_files_processed.txt', 'r') as file:
+        with open('Data-management/zip_files_processed.txt', 'r') as file:
             for line in file:
                 self.processed_folders.add(line.strip())
     def get_missing_zip_folders(self):
@@ -38,27 +41,33 @@ class UpdateDatabase:
                 # Download the ZIP file
                 r = requests.get(ziplink, allow_redirects=True)
                 r.raise_for_status()  # Raise an error for bad responses
-                filename = ziplink.split('/')[-1]
-
+                filename = ziplink.split('/')[-2] + '-' + ziplink.split('_')[-1]
+                print(ziplink.split('/'))
+                print(ziplink.split('_'))
                 # Save the downloaded ZIP file
-                with open(filename, 'wb') as f:
+                with open(f"/tmp/{filename}", 'wb') as f:
                     f.write(r.content)
+                self.created_files.append(f"/tmp/{filename}")
                 print(f"Downloaded and saved as: {filename}")
-
                 # Create a folder name by removing the .zip extension
                 folder_name = filename.replace('.zip', '')
-
                 # Unzip the file
-                with ZipFile(filename, 'r') as zObject:
-                    zObject.extractall(folder_name)
+                with ZipFile(f"/tmp/{filename}", 'r') as zObject:
+                    zObject.extractall(f"/tmp/{folder_name}")
+                self.created_folders.append(f"/tmp/{folder_name}")
                 print(f"Extracted: {filename} to folder: {folder_name}")
 
                 # Optionally, you can keep track of downloaded zip files
-                with open('zip_files_processed.txt', 'a') as processed_file:
+                with open('Data-management/zip_files_processed.txt', 'a') as processed_file:
                     processed_file.write('\n' + ziplink )
-
             except Exception as e:
                 print(f"Error downloading {ziplink}: {e}")
+    def delete_created_files_and_folders(self):
+        for i in range(len(self.created_files)):
+            os.remove(self.created_files[i])
+            print(f"Successfully deleted {self.created_files[i]}")
+            shutil.rmtree(self.created_folders[i])
+            print(f"Successfully deleted {self.created_folders[i]}")
 
 # Example usage
 if __name__ == "__main__":
@@ -66,3 +75,16 @@ if __name__ == "__main__":
     updater.get_already_parsed_folders()
     updater.get_missing_zip_folders()
     updater.download_missing_zip_folders()
+    updater.delete_created_files_and_folders()
+    mongo_client = MongoClient(os.getenv('MONGODB_URI'))
+    collection = mongo_client["Nonprofitly"]['NonProfitData']
+    document = collection.find({ "Nm": "Rensselaer Polytechnic Institute" })
+
+    if document:
+        print("Random document from MongoDB:")
+        print(document)
+    else:
+        print("No documents found in the MongoDB collection.")
+
+    # t = Database()
+    # t.test()
