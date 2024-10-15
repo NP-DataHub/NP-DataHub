dabe8a223914288f1a0c88aad47da9f9fafb59bd
Youssef-Chip <yhassan2004@gmail.com>
2024-10-14 21:49:17 -0400

    test runtime

--- Data-management/db_update_runner.py
+++ Data-management/db_update_runner.py
@@ -12,16 +12,16 @@ if __name__ == "__main__":
     webscraper.download_missing_zip_folders()
     if webscraper.unprocessed_folders:
         # if there is new data to get, add it to our database
-        db = Database()
+        # db = Database()
         cleanup = Cleanup()
-        cleanup.restore_missing_nonprofits()
+        # cleanup.restore_missing_nonprofits()
         cleanup.drop_all_indices()
-        for folder in webscraper.created_folders:
-            db.process_all_xml_files(folder)
-            db.output_duplicates(folder[5:]) # basically remove /tmp
+        # for folder in webscraper.created_folders:
+        #     db.process_all_xml_files(folder)
+        #     db.output_duplicates(folder[5:]) # basically remove /tmp
         # Deleted download zip files, move back incomplete rows to their own table, add back indices and reset ntee table
         cleanup.delete_created_files_and_folders(webscraper.created_files, webscraper.created_folders)
-        cleanup.move_missing_nonprofits()
+        # cleanup.move_missing_nonprofits()
         cleanup.create_indices()
         cleanup.reset_ntee_table()
         # Rebuild ntee table
