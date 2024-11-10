2126a0885821d3cd779f3ced12ac69377d427142
Youssef-Chip <yhassan2004@gmail.com>
2024-11-10 13:50:33 -0500

    tool explanation

--- frontend/src/app/components/CalculatorComponent.js
+++ frontend/src/app/components/CalculatorComponent.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 
 export default function CalculatorSection() {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+    <div className="p-6 bg-[#171821] rounded-lg">
+      <h3 className="text-xl font-semibold text-[#A9DFD8]">Calculator</h3>
+      <p className="text-white ">
+      Compare NTEE code sectors against public data that align with various regional non-profit’s missions.
+      The public data is pulled from the U.S. Census, which offers the strongest baseline across a host of demographic variables.</p>
+      </div>
     );
 }
